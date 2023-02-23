975d416fea4f3e0bd2337bfd4bb57041a5e44cf9
eliashossain001 <elias.hossain191@gmail.com>
2023-02-23 10:00:03 +0600

    update the function

--- n_best_token.py
+++ n_best_token.py
@@ -6,41 +6,42 @@ warnings.filterwarnings("ignore")
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-
 def beam_search_decoder(data, beam_width, n_best):
     df = pd.DataFrame(data)
     data = tf.nn.softmax(df.values)
-    sequences = [[list(), [], 0.0]]
-
+    sequences = [(list(), 0.0)]
     results = []
     for i, row in enumerate(data):
-        all_candidates = list()
-        for seq, seq_log_probs, score in sequences:
+        all_candidates = []
+        for seq, score in sequences:
             for j in range(len(row)):
                 candidate_seq = seq + [j]
-                candidate_seq_log_probs = seq_log_probs + [np.log(row[j])]
-                # compute score
                 next_token_log_prob = np.log(row[j])
-                candidate_score = sum(candidate_seq_log_probs) / len(candidate_seq_log_probs) + next_token_log_prob * len(candidate_seq)
-                #print('candidate score:', candidate_score)
-                candidate = [candidate_seq, candidate_seq_log_probs, candidate_score]
+                candidate_score = score + next_token_log_prob
+                candidate = (candidate_seq, candidate_score)
                 all_candidates.append(candidate)
-        ordered = sorted(all_candidates, key=lambda tup:tup[2], reverse=True)
+        ordered = sorted(all_candidates, key=lambda tup: tup[1], reverse=True)
         sequences = ordered[:beam_width]
-        n_best_sequences = sequences[:n_best]
+        n_best_sequences = [(seq[-1], score) for seq, score in sequences[:n_best]]
         results.append(n_best_sequences)
     
-    # Convert token indices back to the original token names
-    token_names = list(df.columns)
-    final_results = []
-    for step_results in results:
-        step_token_scores = []
-        for seq, seq_log_probs, score in step_results:
-            token_seq = [token_names[i] for i in seq]
-            step_token_scores.append((token_seq[-1], score))
-        final_results.append(step_token_scores)
+        # Create a dictionary to map the token indices to their corresponding phoneme names
+        token_index_to_name = dict(enumerate(range(len(row))))
+        index_to_phoneme = {v: k for k, v in token_index_to_name.items()}
+
+        # Convert token indices back to their corresponding phoneme names
         
+        final_results = []
+        for step_results in results:
+            step_token_scores = []
+            for token_index, score in step_results:
+                # Convert token indices to their corresponding phoneme names
+                token_name = index_to_phoneme[token_index]
+                step_token_scores.append((token_name, score))
+            final_results.append(step_token_scores)
+                    
     return final_results
+
 
 
 
@@ -60,9 +61,9 @@ data = {
 
 df = pd.DataFrame(data)
 
-beam_search_results = beam_search_decoder(df, beam_width=10, n_best=5)
+beam_search_results = beam_search_decoder(df, beam_width=30, n_best=30)
 for i, step_results in enumerate(beam_search_results):
     print(f"Time step {i}:")
     for rank, (token, score) in enumerate(step_results):
+        #print('score:', score)
         print(f"{rank+1}-best Token: {token}, Score: {score}")
-
